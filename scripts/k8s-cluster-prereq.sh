2843be59f3615d15afea1cbce6c9a89322fb9cc5
ashishgoel2411 <ashishgoel2411@gmail.com>
2021-01-31 11:20:45 +0530

    modified jenkinsfile

--- scripts/k8s-cluster-prereq.sh
+++ scripts/k8s-cluster-prereq.sh
@@ -60,17 +60,17 @@ curl -L https://github.com/aquasecurity/kube-bench/releases/download/v0.3.1/kube
 sudo apt install ./kube-bench_0.3.1_linux_amd64.deb -f
 
 #Service account
-cat <<EOF | sudo tee /etc/demoaccount.json
-{
-    "type": "service_account",
-    "project_id": "stoked-genius-302113",
-    "private_key_id": "471d273f90a8371852bd02e3f2c75880fa981245",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDYO8LJfVBmYLv2\n4pFPk69r7P6FwRm3EcQ0mFeO6Z9MPlifmGXCrJymx7EwTJLycGppWstjTdUogsrF\nV+Y4ugqCOurSu3VIYOJTawhUnNt+Yx9IT9WJkqy7NFo3Pjcyoe1FxB1kmAjLEGmz\nPiwDOg2FmzlidSDSbKwX0hnK7Gell18XIgw8lC1HlWYz01yaf2FxTUaCF4hPouHG\n1NeIE3g2T/eeFFUoaO7RsB9H/gdP5IbyhNXklJVbq02wpOfFtoK2U95J8NoQGX9n\ni/HE7xJZgNcrrQ/X/yGEzfgPv/mEZCiQ0JX3zAOD/dAo9vDLU+e54nhxzKpeDZxe\nmQn18gtXAgMBAAECggEAZl4UXzM2RuFw2vZz+IpgN+XgsQ2z0NlWW9frS7sKMRwq\n9JM/SaRSHRrYpZb60k+p8OqM8xw714vj1v0//ocExpaB6PXaFkPXocpADV6dAv6G\nJsO0wgZwQ8gLFbAcjZnfLHIlwCPBtfC1OagrSfPHxwKvbg8pVLUh270ZfNniT5+1\nEMd1Kb25IA2rO9vLKhEseKHRi0aOUh+ybN5LhbyVcBGIJT+ObeKjManSS2IJFtCS\npvEBJpVxY4p+C7/SOcEojfAfv/m5fOFNt7Ta+L7gmp2D6tcess+7andY4mmTIf4H\n4HpHn9jRJvmh0zN968w56fF0pVXipskEC9qulPDBcQKBgQDwUjr7jSbLLPhOBabp\n0W1HL+D5X7+8ERLjgx79nShbdMkEYJUVIVxk37Hz4qDTqcMrkKD+40v7yGhe1IAH\n0KMMRVENGyRKATh1XkIR9VQ6LXgiPgmlcBvpav0gAkk0npjTthc1u+D7/DDWNsUk\nEtrlMu6P+mrFkgXVXFq30FqpfQKBgQDmVzlc0SJXFQ/0zajmc3yvt+g3LVUpKcT6\nP/Efzfj27WYTacqrEORdBgjldz/0r4oVuvBuK1wKMftAIbgCauhOfzKHHqXNo8GU\ntpMKMS8jrJX6e4Q2sC44XP2FYMv1K59SKQrt6/HBDQigIj0DEYjJ1ySAoJ4C/Zsi\nzcbS//SAYwKBgA29ddfaCyTErnGviwqFSc0vJJq9abXz9/OAFK6FZMBN3gxdBUkO\n/jxaOc9q3q0bYKhF/Z51GmwL2kPJvIQ13oyeDfNE4P9GMJctS3PBZg5peRvkFiMa\nVM40qsJAvzS9+JjqKUuOMV/W3Gfy+7SDS3adQdyDgcVIAwNe42TtCbH1AoGBAM3I\no1DO7nUKNiUpD3pDNuPt06Jn96s8y+Qe+BbH92LzmLxDSXMnNj39RDltHlBF+4GK\nb36dci8RlDwpY7GVPVHTxZp9wPdFE47+xcLXShv+9ka/ztDk+YYVXdGpNs4kWMQY\nMtF4d5VXRZt6uzNgmOpqMQxRcpmNZOnlGXosfY2tAoGBAMXqXPVPu+P3Sm/ZJXrG\nqh0ckTi9p0CBEsNTaSzxCtvQMSxhNzX17/0zpobXw/IDV3qTOUhDihRWRfl6A8lM\nugSFpF7KodYg2HYzAldsNBBc3LyFsiMJ9LX5o9L2IYOLTYpcqHz6jlu+8wH2qDl0\nqbSIDkDZO7UfPzLlw+1e5+oO\n-----END PRIVATE KEY-----\n",
-    "client_email": "demoaccount@stoked-genius-302113.iam.gserviceaccount.com",
-    "client_id": "111134476876590081663",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/demoaccount%40stoked-genius-302113.iam.gserviceaccount.com"
-}
-EOF
+#cat <<EOF | sudo tee /etc/demoaccount.json
+#{
+#    "type": "service_account",
+#    "project_id": "stoked-genius-302113",
+#    "private_key_id": "471d273f90a8371852bd02e3f2c75880fa981245",
+#    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDYO8LJfVBmYLv2\n4pFPk69r7P6FwRm3EcQ0mFeO6Z9MPlifmGXCrJymx7EwTJLycGppWstjTdUogsrF\nV+Y4ugqCOurSu3VIYOJTawhUnNt+Yx9IT9WJkqy7NFo3Pjcyoe1FxB1kmAjLEGmz\nPiwDOg2FmzlidSDSbKwX0hnK7Gell18XIgw8lC1HlWYz01yaf2FxTUaCF4hPouHG\n1NeIE3g2T/eeFFUoaO7RsB9H/gdP5IbyhNXklJVbq02wpOfFtoK2U95J8NoQGX9n\ni/HE7xJZgNcrrQ/X/yGEzfgPv/mEZCiQ0JX3zAOD/dAo9vDLU+e54nhxzKpeDZxe\nmQn18gtXAgMBAAECggEAZl4UXzM2RuFw2vZz+IpgN+XgsQ2z0NlWW9frS7sKMRwq\n9JM/SaRSHRrYpZb60k+p8OqM8xw714vj1v0//ocExpaB6PXaFkPXocpADV6dAv6G\nJsO0wgZwQ8gLFbAcjZnfLHIlwCPBtfC1OagrSfPHxwKvbg8pVLUh270ZfNniT5+1\nEMd1Kb25IA2rO9vLKhEseKHRi0aOUh+ybN5LhbyVcBGIJT+ObeKjManSS2IJFtCS\npvEBJpVxY4p+C7/SOcEojfAfv/m5fOFNt7Ta+L7gmp2D6tcess+7andY4mmTIf4H\n4HpHn9jRJvmh0zN968w56fF0pVXipskEC9qulPDBcQKBgQDwUjr7jSbLLPhOBabp\n0W1HL+D5X7+8ERLjgx79nShbdMkEYJUVIVxk37Hz4qDTqcMrkKD+40v7yGhe1IAH\n0KMMRVENGyRKATh1XkIR9VQ6LXgiPgmlcBvpav0gAkk0npjTthc1u+D7/DDWNsUk\nEtrlMu6P+mrFkgXVXFq30FqpfQKBgQDmVzlc0SJXFQ/0zajmc3yvt+g3LVUpKcT6\nP/Efzfj27WYTacqrEORdBgjldz/0r4oVuvBuK1wKMftAIbgCauhOfzKHHqXNo8GU\ntpMKMS8jrJX6e4Q2sC44XP2FYMv1K59SKQrt6/HBDQigIj0DEYjJ1ySAoJ4C/Zsi\nzcbS//SAYwKBgA29ddfaCyTErnGviwqFSc0vJJq9abXz9/OAFK6FZMBN3gxdBUkO\n/jxaOc9q3q0bYKhF/Z51GmwL2kPJvIQ13oyeDfNE4P9GMJctS3PBZg5peRvkFiMa\nVM40qsJAvzS9+JjqKUuOMV/W3Gfy+7SDS3adQdyDgcVIAwNe42TtCbH1AoGBAM3I\no1DO7nUKNiUpD3pDNuPt06Jn96s8y+Qe+BbH92LzmLxDSXMnNj39RDltHlBF+4GK\nb36dci8RlDwpY7GVPVHTxZp9wPdFE47+xcLXShv+9ka/ztDk+YYVXdGpNs4kWMQY\nMtF4d5VXRZt6uzNgmOpqMQxRcpmNZOnlGXosfY2tAoGBAMXqXPVPu+P3Sm/ZJXrG\nqh0ckTi9p0CBEsNTaSzxCtvQMSxhNzX17/0zpobXw/IDV3qTOUhDihRWRfl6A8lM\nugSFpF7KodYg2HYzAldsNBBc3LyFsiMJ9LX5o9L2IYOLTYpcqHz6jlu+8wH2qDl0\nqbSIDkDZO7UfPzLlw+1e5+oO\n-----END PRIVATE KEY-----\n",
+#    "client_email": "demoaccount@stoked-genius-302113.iam.gserviceaccount.com",
+#    "client_id": "111134476876590081663",
+#    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#    "token_uri": "https://oauth2.googleapis.com/token",
+#    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/demoaccount%40stoked-genius-302113.iam.gserviceaccount.com"
+#}
+#EOF
