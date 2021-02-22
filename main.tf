4cd0f06166f2f56310529052933f1bbc1610daeb
ashishgoel2411 <ashishgoel2411@gmail.com>
2021-02-22 22:14:21 +0530

    modified

--- main.tf
+++ main.tf
@@ -66,24 +66,12 @@ resource "google_compute_network_peering" "peering1" {
   name         = "peering1"
   network      = google_compute_network.default.id
   peer_network = google_compute_network.k8s-vnet-tf.id
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral IP
-    }
-  }  
 }
 
 resource "google_compute_network_peering" "peering2" {
   name         = "peering2"
   network      = google_compute_network.k8s-vnet-tf.id
   peer_network = google_compute_network.default.id
-    network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral IP
-    }
-  } 
 }
 
 resource "google_dns_managed_zone" "peering-zone" {
